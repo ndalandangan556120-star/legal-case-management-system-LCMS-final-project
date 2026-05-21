8b2606d326798bf589b39b97c577d6f9ef3407b8
Nasrullah M. Dalandangan <n.dalandangan.556120@umindanao.edu.ph>
2026-05-21 13:44:00 +0800

    Fix root redirect and Render Docker Apache path

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/dashboard');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
